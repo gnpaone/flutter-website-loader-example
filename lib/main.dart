88fb27c68b64d2453d87e1f8a6507741ea09bcbb
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2023-07-26 17:33:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_universal/webview_universal.dart';
 
 class ReactWebsite extends StatefulWidget {
-  final String currentPlanet;
-
-  ReactWebsite({required this.currentPlanet});
-
   @override
   _ReactWebsiteState createState() => _ReactWebsiteState();
 }
 
 class _ReactWebsiteState extends State<ReactWebsite> {
-  WebViewController? _webViewController;
+  WebViewController _webViewController = WebViewController();
+
+  @override
+  void initState() {
+    super.initState();
+    _task();
+  }
+
+  Future<void> _task() async {
+    await _webViewController.init(
+      context: context,
+      setState: (void Function() fn) {},
+      uri: Uri.parse('https://gnpaone.github.io/spaceExplore/'),
+    );
+
+    // Wait for the WebView to be fully loaded
+    await Future.delayed(Duration(seconds: 1));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebView(
-        initialUrl: 'https://gnpaone.github.io/spaceExplore/', // Replace this with the actual URL
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (controller) {
-          _webViewController = controller;
-          _updateCurrentPlanet();
-        },
+        controller: _webViewController,
       ),
-    );
-  }
-
-  void _updateCurrentPlanet() async {
-    // Wait for the WebView to be fully loaded
-    await Future.delayed(Duration(seconds: 1));
-
-    // Execute JavaScript code to set the currentPlanet value in the loaded website
-    await _webViewController?.evaluateJavascript(
-      "window.currentPlanet = '${widget.currentPlanet}';",
     );
   }
 }
@@ -42,9 +40,10 @@ class _ReactWebsiteState extends State<ReactWebsite> {
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .then((_) {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
     runApp(MyApp());
   });
 }
@@ -53,11 +52,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ReactWebsite(
-        currentPlanet: 'uranus',
-      ),
+      home: ReactWebsite(),
     );
   }
 }
-
-
